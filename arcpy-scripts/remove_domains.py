da1aff633f8eac67f880e56b1d1d67bf265635d1
Amiya Kumar <amiyakumarbag@gmail.com>
2026-01-08 14:14:31 +0530

    Add usage documentation to remove_domains script

--- arcpy-scripts/remove_domains.py
+++ arcpy-scripts/remove_domains.py
@@ -1,4 +1,13 @@
 """
+Script: remove_domains.py
+Purpose: Remove coded value domains from fields
+Author: Amiya Kumar
+
+Usage:
+1. Update feature_class path
+2. Run script in ArcGIS Pro Python environment
+"""
+"""
 Remove domains from multiple fields in a feature class
 Useful when field values do not update due to domains
 Author: Amiya Kumar
